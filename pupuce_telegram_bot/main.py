6be953e39ae851e7cce5f8acebdf7a1c30950631
Alby Ponomareva <klemlemony13@gmail.com>
2023-03-16 17:55:35 +0530

    commit_3

--- pupuce_telegram_bot/main.py
+++ pupuce_telegram_bot/main.py
@@ -2,10 +2,8 @@
 
 # сообщения должны приходить в общий чат
 # если от бота два сообщения подряд, то между ними должен быть лаг в 1 секунду
+# если вызываешь пупуся сам, он должен недовольно ответить, что иди нахуй - добавить хэндлер поверх всего, где будет просто текстово вызываться пупусь
 
-# если вызываешь пупуся сам, он должен недовольно ответить, что иди нахуй
-
-# проверка
 
 from aiogram import Bot, Dispatcher, types, executor
 from config import TOKEN_API
@@ -14,9 +12,10 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 import asyncio
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from datetime import datetime, timedelta #timedelta возможно удали
 
 storage: MemoryStorage = MemoryStorage()
-scheduler = AsyncIOScheduler()
+scheduler = AsyncIOScheduler(timezone='Asia/Colombo')
 
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot,
@@ -29,25 +28,24 @@ class Form(StatesGroup):
     is_everything_ok = State()
     remember_oath = State()
 
-@dp.message_handler()
-async def start_cmd_handler():
-    await Form.is_everything_ok.set()
-    await bot.send_message(chat_id = -954441708, text='У тебя все хорошо?')
+async def start_cmd_handler(bot: Bot):
+    await bot.send_message(-954441708, f'У тебя все хорошо? Да или нет?')
 
-@dp.message_handler(state=Form.is_everything_ok)
-async def process_is_everything_ok(message: types.Message, state: FSMContext):
-    if message.text.lower() == 'да':
-        await message.answer("ну и чудненько")
-        await asyncio.sleep(5)
-        await Form.remember_oath.set()
-        await message.answer("Клятва на пальчиках в силе? Мы еще охана?")
-    elif message.text.lower() == 'нет':
-        await message.answer("если честно, мне все равно. Я просто спросил")
-        await asyncio.sleep(5)
-        await Form.remember_oath.set()
-        await message.answer("Клятва на пальчиках в силе? Мы еще охана?")
-    else:
-        await message.answer("чего блять? Ты пожешь написать да или нет?")
+@dp.message_handler(text='да')
+async def start_cmd(message: types.Message):
+    await Form.is_everything_ok.set()
+    await bot.send_message(chat_id = -954441708, text='ну и чудненько')
+    await asyncio.sleep(2)
+    await Form.remember_oath.set()
+    await message.answer("Клятва на пальчиках в силе? Мы еще охана?")
+
+@dp.message_handler(text='нет')
+async def start_cmd(message: types.Message):
+    await Form.is_everything_ok.set()
+    await bot.send_message(chat_id = -954441708, text='если честно, мне все равно. Я просто спросил')
+    await asyncio.sleep(2)
+    await Form.remember_oath.set()
+    await message.answer("Клятва на пальчиках в силе? Мы еще охана?")
 
 @dp.message_handler(state=Form.remember_oath)
 async def process_remember_oath(message: types.Message, state: FSMContext):
@@ -60,7 +58,23 @@ async def process_remember_oath(message: types.Message, state: FSMContext):
 
     await state.finish()
 
-scheduler.add_job(start_cmd_handler, 'interval', seconds=5)
+@dp.message_handler(text='пупусь')
+async def pupuce(message: types.Message):
+    await message.answer("че надо? Иди нахуй")
+    await asyncio.sleep(1)
+    await bot.send_sticker(chat_id = -954441708, sticker='CAACAgIAAxkBAAEIKHFkEwiKU-xDCnNIoNFwUq37X88AAVgAAjYJAAJ5XOIJQ8ZQFdq6zjcvBA')
+
+@dp.message_handler()
+async def i_do_not_understand_you(message: types.Message):
+    await message.answer("Отвали")
+    await asyncio.sleep(1)
+    await bot.send_sticker(chat_id = -954441708, sticker='CAACAgIAAxkBAAEIKHNkEwnuXZE78S7NqncS2y4w0G4ylAACEgADNlhqEvnhF8xf8PntLwQ')
+
+scheduler.add_job(start_cmd_handler, 'cron',
+                  day_of_week=3,
+                  hour=17,
+                  minute=51,
+                  kwargs={'bot': bot})
 scheduler.start()
 
 if __name__ == '__main__':
