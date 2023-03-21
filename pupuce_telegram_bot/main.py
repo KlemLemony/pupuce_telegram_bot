a67d2ab87dc839ecb4b22e5cef94b9ff558bc600
Alby Ponomareva <klemlemony13@gmail.com>
2023-03-21 14:04:33 +0530

    commit_5

--- pupuce_telegram_bot/main.py
+++ pupuce_telegram_bot/main.py
@@ -5,6 +5,8 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 import asyncio
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+import random
+from random import randint
 
 storage = MemoryStorage()
 bot = Bot(TOKEN_API)
@@ -20,7 +22,7 @@ class Form(StatesGroup):
 
 async def start_cmd_handler(bot: Bot):
     await bot.send_message(-1001610055755, f'У тебя все хорошо? Да или нет?')
-    await dp.current_state(chat=-1001610055755, user=335261939).set_state(Form.is_everything_ok)
+    await dp.current_state(chat=-1001610055755, user=['433003376', '335261939']).set_state(Form.is_everything_ok)
 
 @dp.message_handler(text='да', state=Form.is_everything_ok)
 async def start_cmd(message: types.Message, state: FSMContext):
@@ -39,6 +41,8 @@ async def start_cmd(message: types.Message, state: FSMContext):
 @dp.message_handler(state=Form.is_everything_ok)
 async def start_cmd(message: types.Message, state: FSMContext):
     await bot.send_message(chat_id = -1001610055755, text='я тебя не понимаю')
+    #await asyncio.sleep(1)
+    await bot.send_sticker(chat_id = -1001610055755, sticker='CAACAgIAAxkBAAEIKHNkEwnuXZE78S7NqncS2y4w0G4ylAACEgADNlhqEvnhF8xf8PntLwQ')
 
 @dp.message_handler(text='да', state=Form.remember_oath)
 async def start_cmd(message: types.Message, state: FSMContext):
@@ -54,17 +58,17 @@ async def start_cmd(message: types.Message, state: FSMContext):
 async def start_cmd(message: types.Message, state: FSMContext):
     await bot.send_message(chat_id = -1001610055755, text='чего блять? Ты пожешь написать да или нет?')
     
-@dp.message_handler(text=['пупусь','pupuce', 'пу', 'Пупусь','Pupuce', 'Пу'])
+@dp.message_handler(text=['пупусь','pupuce', 'пу', 'Пупусь','Pupuce', 'Пу', 'Пупусь!', 'пупусь!'])
 async def pupuce(message: types.Message):
     await message.answer("че надо? Иди нахуй")
     await asyncio.sleep(1)
     await bot.send_sticker(chat_id = -1001610055755, sticker='CAACAgIAAxkBAAEIKHFkEwiKU-xDCnNIoNFwUq37X88AAVgAAjYJAAJ5XOIJQ8ZQFdq6zjcvBA')
 
-@dp.message_handler()
-async def go_fuck_yourself(message: types.Message):
-    await message.answer("Отвали, я не хочу разговаривать")
-    await asyncio.sleep(1)
-    await bot.send_sticker(chat_id = -1001610055755, sticker='CAACAgIAAxkBAAEIKHNkEwnuXZE78S7NqncS2y4w0G4ylAACEgADNlhqEvnhF8xf8PntLwQ')
+#@dp.message_handler()
+#async def go_fuck_yourself(message: types.Message):
+#    await message.answer("Отвали, я не хочу разговаривать")
+#    await asyncio.sleep(1)
+#    await bot.send_sticker(chat_id = -1001610055755, sticker='CAACAgIAAxkBAAEIKHNkEwnuXZE78S7NqncS2y4w0G4ylAACEgADNlhqEvnhF8xf8PntLwQ')
 
 #scheduler.add_job(start_cmd_handler, trigger='cron',
 #                  day_of_week=1,
@@ -72,8 +76,10 @@ async def go_fuck_yourself(message: types.Message):
 #                  minute=7,
 #                  kwargs={'bot': bot})
 
+n = random.randint(10, 61)
+
 scheduler.add_job(start_cmd_handler, trigger='interval',
-                  minutes=30,
+                  minutes=n,
                   kwargs={'bot': bot})
 
 scheduler.start()
