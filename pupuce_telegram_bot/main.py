48313bc9cce61b8273f51bf6c68b8b824dca1ce5
Alby Ponomareva <klemlemony13@gmail.com>
2023-03-31 16:24:37 +0530

    next_commit

--- pupuce_telegram_bot/main.py
+++ pupuce_telegram_bot/main.py
@@ -21,48 +21,48 @@ class Form(StatesGroup):
     remember_oath = State()
 
 async def start_cmd_handler(bot: Bot):
-    await bot.send_message(-1001610055755, f'У тебя все хорошо? Да или нет?')
-    await dp.current_state(chat=-1001610055755, user=['433003376', '335261939']).set_state(Form.is_everything_ok)
+    await bot.send_message(-954441708, f'У тебя все хорошо? Да или нет?')
+    await dp.current_state(chat=-954441708, user=335261939).set_state(Form.is_everything_ok)
 
 @dp.message_handler(text='да', state=Form.is_everything_ok)
 async def start_cmd(message: types.Message, state: FSMContext):
-    await bot.send_message(chat_id = -1001610055755, text='ну и чудненько')
+    await bot.send_message(chat_id = -954441708, text='ну и чудненько')
     await asyncio.sleep(2)
     await message.answer("Клятва на пальчиках в силе? Мы еще охана?")
     await Form.next()
 
 @dp.message_handler(text='нет', state=Form.is_everything_ok)
 async def start_cmd(message: types.Message, state: FSMContext):
-    await bot.send_message(chat_id = -1001610055755, text='если честно, мне все равно. Я просто спросил')
+    await bot.send_message(chat_id = -954441708, text='если честно, мне все равно. Я просто спросил')
     await asyncio.sleep(2)
     await message.answer("Клятва на пальчиках в силе? Мы еще охана?")
     await Form.next()
 
 @dp.message_handler(state=Form.is_everything_ok)
 async def start_cmd(message: types.Message, state: FSMContext):
-    await bot.send_message(chat_id = -1001610055755, text='я тебя не понимаю')
+    await bot.send_message(chat_id = -954441708, text='я тебя не понимаю')
     #await asyncio.sleep(1)
-    await bot.send_sticker(chat_id = -1001610055755, sticker='CAACAgIAAxkBAAEIKHNkEwnuXZE78S7NqncS2y4w0G4ylAACEgADNlhqEvnhF8xf8PntLwQ')
+    await bot.send_sticker(chat_id = -954441708, sticker='CAACAgIAAxkBAAEIKHNkEwnuXZE78S7NqncS2y4w0G4ylAACEgADNlhqEvnhF8xf8PntLwQ')
 
 @dp.message_handler(text='да', state=Form.remember_oath)
 async def start_cmd(message: types.Message, state: FSMContext):
-    await bot.send_message(chat_id = -1001610055755, text='япикаееееей, мазафака. Тогда до встречи на следующей неделе! Счастья, здоровья тебе')
+    await bot.send_message(chat_id = -954441708, text='япикаееееей, мазафака. Тогда до встречи на следующей неделе! Счастья, здоровья тебе')
     await state.finish()
 
 @dp.message_handler(text='нет', state=Form.remember_oath)
 async def start_cmd(message: types.Message, state: FSMContext):
-    await bot.send_message(chat_id = -1001610055755, text='Пидор')
+    await bot.send_message(chat_id = -954441708, text='Пидор')
     await state.finish()
 
 @dp.message_handler(state=Form.remember_oath)
 async def start_cmd(message: types.Message, state: FSMContext):
-    await bot.send_message(chat_id = -1001610055755, text='чего блять? Ты пожешь написать да или нет?')
+    await bot.send_message(chat_id = -954441708, text='чего блять? Ты пожешь написать да или нет?')
     
 @dp.message_handler(text=['пупусь','pupuce', 'пу', 'Пупусь','Pupuce', 'Пу', 'Пупусь!', 'пупусь!'])
 async def pupuce(message: types.Message):
     await message.answer("че надо? Иди нахуй")
     await asyncio.sleep(1)
-    await bot.send_sticker(chat_id = -1001610055755, sticker='CAACAgIAAxkBAAEIKHFkEwiKU-xDCnNIoNFwUq37X88AAVgAAjYJAAJ5XOIJQ8ZQFdq6zjcvBA')
+    await bot.send_sticker(chat_id = -954441708, sticker='CAACAgIAAxkBAAEIKHFkEwiKU-xDCnNIoNFwUq37X88AAVgAAjYJAAJ5XOIJQ8ZQFdq6zjcvBA')
 
 #@dp.message_handler()
 #async def go_fuck_yourself(message: types.Message):
@@ -76,10 +76,8 @@ async def pupuce(message: types.Message):
 #                  minute=7,
 #                  kwargs={'bot': bot})
 
-n = random.randint(10, 61)
-
 scheduler.add_job(start_cmd_handler, trigger='interval',
-                  minutes=n,
+                  minutes=10,
                   kwargs={'bot': bot})
 
 scheduler.start()
